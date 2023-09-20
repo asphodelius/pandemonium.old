84513d56993196e50f35f4c1bc8c51f163381cf4
asphodelius <sudorgin.n.boss@gmail.com>
2023-09-20 10:24:34 +0700

    added preloader

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Simulate a delay for demonstration purposes (remove this in production)
+  setTimeout(function () {
+    // Hide the preloader
+    document.querySelector(".preloader-container").style.display = "none";
+  }, 2000); // Adjust the delay time as needed
+});
